8ed3e794588e9277f27ae2b0ba35968253017e41
Justin Collins <justin@presidentbeef.com>
2013-12-01 21:47:10 -0800

    Add check for Parameters#permit!

--- lib/brakeman/warning_codes.rb
+++ lib/brakeman/warning_codes.rb
@@ -62,7 +62,8 @@ module Brakeman::WarningCodes
     :unsafe_symbol_creation => 59,
     :dangerous_attr_accessible => 60,
     :local_request_config => 61,
-    :detailed_exceptions => 62
+    :detailed_exceptions => 62,
+    :mass_assign_permit! => 63,
   }
 
   def self.code name
